bd732d31233cef90992145a0696bdb3915dc4042
JamesLBrocklehurst <jameslbrocklehurst@gmail.com>
2026-04-06 11:18:29 +0100

    Changed .gitignore file and updated ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "this" {
   ami                     = "ami-0f1b092c39d616d45"
   instance_type           = "t2.nano"
-  subnet_id               = "var.subnet_id"
+  subnet_id               = var.subnet_id
 }
