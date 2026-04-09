0b97549f222711d72cfa2407413055903b05b907
JamesLBrocklehurst <jameslbrocklehurst@gmail.com>
2026-04-09 17:53:13 +0100

    Created modules folder and added/moved the ec2.tf file

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -1,5 +1,6 @@
 resource "aws_instance" "this" {
   ami                     = "ami-0f1b092c39d616d45"
-  instance_type           = "t2.nano"
+  instance_type           = var.instance_type
   subnet_id               = var.subnet_id
 }
+
